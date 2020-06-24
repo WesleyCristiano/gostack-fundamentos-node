3ea14d1f66103e8f96fd6b686c09f13dcc64bba5
Wesley Cristiano <wsley@hotmail.com>
2020-06-24 00:57:14 -0400

    resolução do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,7 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +9,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    if((type =='outcome') && (value> this.transactionsRepository.getBalance().total)){
+      throw Error('Desculpe, seu saldo é insuficiente')
+      }
+    const transaction = this.transactionsRepository.create({title, value, type})
+    return transaction
   }
 }
 
